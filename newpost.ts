c31c073b70d605f94535d20895ca5fada84f36f2
e47556 <e47556@gmail.com>
2025-08-28 00:45:00 +0900

    add 여주 없는 세계의 악역영애는 혼약을 파기하고 강아지과 시종과 도망친다

---
+++ newpost.ts
@@ -0,0 +1,52 @@
+import { exec, execSync } from 'node:child_process';
+
+import * as cheerio from 'cheerio';
+import { writeFileSync } from 'node:fs';
+import path from 'node:path';
+
+async function main() {
+    const args = process.argv.slice(2).join(' ').trim();
+    let name = args;
+    const ridiPrefix = 'https://ridibooks.com/books/';
+    const isRidi = name.startsWith(ridiPrefix);
+
+    if (isRidi) {
+        const res = await fetch(name);
+
+        const html = await res.text();
+        const $ = cheerio.load(html);
+
+        const ogTitle = $('meta[property="og:title"]').attr('content');
+        if (!ogTitle)
+            throw new Error('No og:title meta tag found');
+
+        console.log(ogTitle);
+
+        name = ogTitle;
+    }
+
+    const escapedName = name
+        .replaceAll(' ', '_')
+        .replaceAll('?', '');
+
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+
+    console.log(formattedDate);
+
+    execSync(`hugo new content ./content/posts/${formattedDate}-${escapedName}/index.md`, { cwd: __dirname, stdio: 'inherit' });
+
+    if (isRidi) {
+        const imgPath = path.join(__dirname, `./content/posts/${formattedDate}-${escapedName}/cover.webp`)
+        const imgUrl = `https://img.ridicdn.net/cover/${args.substring(ridiPrefix.length)}/xxlarge?dpi=xxhdpi#1`;
+
+        const res = await fetch(imgUrl);
+
+        writeFileSync(imgPath, await res.bytes());
+    }
+}
+
+main().catch(e => console.error(e));
